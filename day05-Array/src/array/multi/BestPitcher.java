b141ce664ea72861c6e9f9e0b0a82a921da4900e
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-07-11 15:49:04 +0900

    배열 문제(아직 미완성)

---
+++ day05-Array/src/array/multi/BestPitcher.java
@@ -0,0 +1,45 @@
+package array.multi;
+
+import java.util.Scanner;
+
+/**
+ * 야구 구단의 1 ~ 5선발 선수의 방어률을 입력하고 
+ * 그 중, 가장 방어율이 낮은 최고의 투수를 찾는 클래스
+ * 
+ * @author Administrator
+ *
+ */
+public class BestPitcher {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double[][] era = new double[3][5];
+		double min = 10.0;
+		double score;
+
+		// 각 팀별로 각 투수의 방어률 입력받아 배열에 저장한다.
+		System.out.println("1선발 2선발 3선발 4선발 5선발 방어률 입력");
+
+		for (int idx = 0; idx < era.length; idx++) {
+
+			for (int idn = 0; idn < era[idx].length; idn++) {
+
+				System.out.println((idx + 1) + "번째 팀" + (idn + 1) + "번째 선수의 방어률은 : ");
+
+				score = scan.nextDouble();
+				
+				if(score < min) {
+					
+					min = score;
+					System.out.println("");
+					
+				}else {
+					
+				}
+
+			}
+
+		}
+	}
+
+}
