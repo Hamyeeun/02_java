a2e3cb265b077e97db6b39dec5875f086910df26
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-07-17 15:50:49 +0900

    오레오 쿠키 클래스 미완성(숙)

---
+++ day09-Class.Eraser/src/snack/OreoTest.java
@@ -0,0 +1,35 @@
+package snack;
+/**
+ * Oreo 쿠키가 3 개 들어있는 oreoBox 라는 변수를
+ * Oreo 배열 타입으로 선언하고,
+ * 
+ * oreoBox[0] ~ [2] Oreo 쿠키를 입력
+ * 크림의 종류는 바닐라, 초코, 딸기, 말차 중 하나로 입력할 것
+ * 
+ * foreach를 사용해서
+ * oreoBox에서 모든 쿠키의 상태를 출력
+ * 
+ * for를 사용하여 모든 쿠키의 크림을 바닐아로 변경
+ * 
+ * foreach 사용해서 모든 쿠키의 크림 변경 상태 확인을 위한 출력
+ * @author Administrator
+ *
+ */
+public class OreoTest {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		// Oreo 배열 선언
+		Oreo[] oreoBox;
+		
+//		// 클래스 선언
+//		Oreo oreo = new oreo
+		
+		// 2. 초기화
+		// 배열 초기화
+		oreoBox = new Oreo[3];
+		
+		
+	}
+
+}
