dc8cc2c0f18fa3c6cfb214ef8aedb14e31222216
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-07-22 13:11:20 +0900

    NamePoint추가

--- day12-Inheritance/src/point/PointTest.java
+++ day12-Inheritance/src/point/PointTest.java
@@ -11,10 +11,12 @@ public class PointTest {
 		// 1. 변수선언
 		Point p;
 		Point q;
+		Point o;
 		
 		// 2. 초기화
 		p = new Point(0, 0);
 		q = new Point(3, 4);
+		o = new NamedPoint("O", 0, 0);
 		
 		// 3. 사용
 		// (1) 점 p 를 출력
@@ -22,9 +24,15 @@ public class PointTest {
 	
 		// (2) 점 q 를 출력
 		System.out.println(q);
-	
-		// (3) 두 점 사이의 거리를 계산
+		
+		// (3) 점 o 를 출력
+		System.out.println(o);
+		
+		// (4) 두 점 사이의 거리를 계산
 		System.out.printf("두 점사이의 거리 : %5.2f%n", p.distanceFrom(q));
+		
+		// (5) 이름이 붙은 원점과 점 q 의 거리 계산
+		System.out.printf("원점과 점 q 의 거리 : %5.2f%n", o.distanceFrom(q));
 	
 	}
 
