927e0a9fe3b5a1beaf5dc9757ce65043ea0caf0e
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-07-23 15:55:41 +0900

    오늘의 숙제(Product)

---
+++ day13-Product/src/product/Product.java
@@ -0,0 +1,157 @@
+package product;
+/**
+ * 전자제품을 판매하는 매장에서
+ * 관리하는 대상인 "제품"을  정의하는 클래스
+ * --------------------------------
+ * 멤버변수(private, protected 붙이지 x)
+ * 제품번호 : pcode    : String : <== "P001"
+ * 제품 명   : pname    : String : <== "삼성 지펠..."
+ * 가격       : price    : int    : 진짜 그 제품의 가격
+ * 재고수량 : quantity : int
+ * --------------------------------------------
+ * 생성자(public 으로 선언)
+ * 기본생성자, 매개변수 받는 생성자 필드 1씩 늘려가며
+ * this, this() 사용
+ * --------------------------------------------
+ * 메소드(public 으로 선언)
+ * print() : void
+ *    => 제품의 상태를 출력
+ * toString() : String
+ *    => 이 제품을 대표하는 문자열을 리턴
+ *    => String.format() 으로 문자열 생성후 리턴
+ * discount(double percentage) : int
+ *    => 입력된 퍼센트 만큼 할인돈 가격을 리턴
+ *    
+ * sell(int amount) : void
+ *    => 매장에서 판매(출고)되어
+ *    => 재고수량(quantity)이 amount 만큼 줄어들도록 반영
+ *    => 재고수량이 0보다 작아질 수 없도록 제한
+ *    
+ * buy(int amount) : void
+ *    => 매장에 입고되어 재고수량(quantity)이
+ *    => amount 만큼 늘어나도록 반영 
+ * ===================================================
+ * 작성시 주의할 것
+ * 
+ * 각 필드를 선언할 때, 필드에 대한 설명을 문서주석으로 남기시오
+ * 
+ * 각 생성자 선언할 때, 문서주석 달 것
+ * 
+ * 각 메소드 선언할 때, ㅁ[소드 설명을 문서 주석으로 달 것
+ * 
+ * 테스트는 아직 작성하지 말것
+ *     
+ * @author Administrator
+ *
+ */
+public class Product {
+	
+	// 1. 멤버변수 선언
+	/** 제품번호 */
+	String pcode;
+	/** 제품 명 */
+	String pname;
+	/** 제품 가격 */
+	int price;
+	/** 제품 재고 수량 */
+	int quantity;
+	
+	// 2. 생성자
+	/**
+	 * 기본 생성자
+	 */
+	public Product(){
+		
+	}
+	
+	/**
+	 * Product 클래스의 pcode 멤버변수만 초기화
+	 * @param pcode
+	 */
+	public Product(String pcode) {
+		this();
+		this.pcode = pcode;
+	}
+	
+	/**
+	 * Product 클래스의 pcode, pname을 초기화
+	 * @param pcode
+	 * @param pname
+	 */
+	public Product(String pcode, String pname) {
+		this(pcode);
+		this.pname = pname;
+	}
+	
+	/**
+	 * Product 클래스의 pcode, pname, price 초기화
+	 * @param pcode
+	 * @param pname
+	 * @param price
+	 */
+	public Product(String pcode, String pname, int price) {
+		this(pcode, pname);
+		this.price = price;
+	}
+	
+	/**
+	 * Product 클래스의 모든 멤버 변수를 초기화
+	 * @param pcode
+	 * @param pname
+	 * @param price
+	 * @param quantity
+	 */
+	public Product(String pcode, String pname, int price, int quantity) {
+		this(pcode, pname, price);
+		this.quantity = quantity;
+	}
+	
+	// 3. 메소드
+	/**
+	 * toString() 메소드정의
+	 * 
+	 */
+	@Override
+	public String toString(){
+		return String.format("제품 코드 : %s, 제품 명 : %s, 가격: %d, 재고량 : %d", pcode, pname, price, quantity);
+	}
+	
+	/**
+	 * 이 제품의 모든 정보를 프린트하는 메소드이다.
+	 */
+	public void print() {
+		System.out.println(this);
+	}
+	
+	/**
+	 * discount(double percentage) : int
+	 *    => 입력된 퍼센트 만큼 할인돈 가격을 리턴
+	 * @param percentage
+	 * @return
+	 */
+	public int discount(double percentage) {
+		return price * (int)percentage;
+	}
+	
+	/**
+	 * sell(int amount) : void
+	 *    => 매장에서 판매(출고)되어
+	 *    => 재고수량(quantity)이 amount 만큼 줄어들도록 반영
+	 *    => 재고수량이 0보다 작아질 수 없도록 제한
+	 * @param amount
+	 */
+	public void sell(int amount) {
+		
+	}
+	
+	/**
+	 * buy(int amount) : void
+	 *    => 매장에 입고되어 재고수량(quantity)이
+	 *    => amount 만큼 늘어나도록 반영 
+	 * @param amount
+	 */
+	public void buy(int amount) {
+		
+	}
+	// 4. 접근자, 수정자
+}
